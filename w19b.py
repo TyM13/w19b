dd908e809919b555224bc969bc26086a0c756286
Your Name <you@example.com>
2022-09-24 09:31:55 -0600

    w19b

--- w19b.py
+++ w19b.py
@@ -3,72 +3,38 @@ print('1: Addition')
 print('2: Subtraction')
 print('3: Multiplication')
 print('4: Division')
+
+def add_user_num(user_num_one, user_num_two):
+    return user_num_one + user_num_two
+   
+def sub_user_num(user_num_one, user_num_two):
+    return user_num_one - user_num_two
+
+def mult_user_num(user_num_one, user_num_two):
+    return user_num_one * user_num_two
+
+def div_user_num(user_num_one, user_num_two):
+    return user_num_one / user_num_two
+
+
+
 users_choice = input('enter your selection:')
+users_choice = int(users_choice)
+
+user_num_one = input('enter a number:')
+user_num_one = int(user_num_one)
+user_num_two = input('enter a number:')
+user_num_two = int(user_num_two)
+
+
+#addition-----------
 if(users_choice == 1):
-    print()
-  
-    #addition
-def add_user_num():
-    user_choice = input('enter a number')
-    user_choice = float(user_choice)
-
-    add_user_num_one = add_user_num()
-    add_user_num_two = add_user_num()
-    results = add_user_num_one + add_user_num_two
-    print(results)
-
-
-
-
-    #Subtraction
-def sub_user_num():
-    user_choice = input('enter a number')
-    user_choice = float(user_choice)
-
-    sub_user_num_one = sub_user_num()
-    sub_user_num_two = sub_user_num()
-    results = sub_user_num_one - sub_user_num_two
-    print(results)
-
-
-
-  
-    #Multiplication
-def mult_user_num():
-    user_choice = input('enter a number')
-    user_choice = float(user_choice)
-
-    mult_user_num_one = mult_user_num()
-    mult_user_num_two = mult_user_num()
-    results = mult_user_num_one * mult_user_num_two
-    print(results)
-
-
-
-
-    #Division
-def div_user_num():
-    user_choice = input('enter a number')
-    user_choice = float(user_choice)
-
-    div_user_num_one = div_user_num()
-    div_user_num_two = div_user_num()
-    results = div_user_num_one / div_user_num_two
-    print(results)
-
-
-    
-  
-    # def user_num():
-    #     user_choice = input('enter a number:')
-    #     try:
-    #         user_choice = float(user_choice)
-    #     except ValueError:
-    #         print('you entered a invalid value')
-    #         user_choice = user_num()
-    #     except:
-    #         print('something went wrong')     
-    #         user_choice = user_num() #recursion gets rid of the first call and lest you overwrite it if it was invalid
-    #     return user_choice
-
-    
+    add_user_num(user_num_one, user_num_two)
+    print(user_num_one + user_num_two)
+#Subtraction----------
+elif(users_choice == 2):
+    sub_user_num
+elif(users_choice == 3):
+    mult_user_num
+elif(users_choice == 4):
+    div_user_num 
